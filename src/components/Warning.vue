43676b2d5e27f7db607613b7c8a64cba97f84b02
teerryn <slaughterer90@gmail.com>
2021-06-17 12:04:25 +0100

    With computed

--- src/components/Warning.vue
+++ src/components/Warning.vue
@@ -1,17 +1,19 @@
 <template>
-  <div>Is Value bigger then 4 {{ warningStore.isBigger(4) }}</div>
+  <div>Is Value bigger then 4 {{ isBigger }}</div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { computed, defineComponent } from 'vue';
 import useWarningStore from '../stores/warningStore';
 
 export default defineComponent({
   setup() {
     const warningStore = useWarningStore();
-    return { warningStore };
+
+    const isBigger = computed(() => {
+      return warningStore.isBigger(4);
+    });
+    return { isBigger };
   },
 });
 </script>
-
-<style scoped></style>
